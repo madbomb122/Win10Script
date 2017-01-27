7ca5a5afb521d3819fe97c1aaced244e874e9def
madbomb122 <adam00783@gmail.com>
2017-01-27 15:17:22 -0800

    Update and rename Win10-v2.2-Mod.ps1 to Win10-v2.2.1-Mod.ps1

--- Win10-v2.2.1-Mod.ps1
+++ Win10-v2.2.1-Mod.ps1
@@ -9,7 +9,7 @@
 # Modded Script Info
 # Author: Madbomb122
 # Website: https://github.com/madbomb122/Win10Script
-# Version: 2.2-Mod, 2017-01-26
+# Version: 2.2.1-Mod, 2017-01-27
 ##########
 
 # You can run the script with a -Set WD or -Set WindowsDefault 
