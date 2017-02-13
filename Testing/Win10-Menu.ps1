b55cf108297865c82e3a050bdd224ef951e67d81
madbomb122 <madbomb122@users.noreply.github.com>
2017-02-13 12:08:58 -0800

    Update Win10-Menu.ps1

--- Testing/Win10-Menu.ps1
+++ Testing/Win10-Menu.ps1
@@ -56,36 +56,47 @@ $RelType = "Beta   "
 ##########
 
 ##########
-# Menu Display Items -Start
+# Multi Use Functions -Start
 ##########
 
-$MainMenuItems = @(
-'                    Main Menu                     ',
-'1. Run Script          ','H. Help                ',
-'2. Script Settings     ','C. Copyright           ',
-'3. Load Setting        ','A. About/Version       ',
-'4. Save Setting        ','                       ',
-'5. Script Option       ','                       ',
-'Q. Exit/Quit                                      '
-)
+# Used to Help remove the Automatic variables
+function cmpv {
+    Compare-Object (Get-Variable) $AutomaticVariables -Property Name -PassThru | Where -Property Name -ne "AutomaticVariables"
+}
 
-$ScriptSettingsMainMenuItems = @(
-'             Script Setting Main Menu             ',
-'1. Privacy Settings    ','7. Windows Update      ',
-'2. Service Tweaks      ','8. Context Menu        ',
-'3. Start Menu          ','9. Task Bar            ',
-'4. Lock Screen         ','10. Features           ',
-"5. 'This PC'           ",'11. Metro Apps         ',
-'6. Explorer            ','12. Misc/Photo Viewer  ',
-'B. Back to Main Menu                              '
-)
+function ChoicesMenu($Vari,$Number) {
+    $VariJ = -join($Vari,"Items")
+    $VariV = Get-Variable $Vari -valueOnly #Variable
+    $VariA = Get-Variable $VariJ -valueOnly #Array
+    $ChoicesMenu = 'X'
+    while($ChoicesMenu -ne "Out"){
+        Clear-Host
+		If($Invalid -eq 1){
+		    Write-host ""
+			Write-host "Invalid Selection" -ForegroundColor Red -BackgroundColor Black -NoNewline
+			$Invalid = 0
+		}
+		ChoicesDisplay $VariA $VariV
+        $ChoicesMenu = Read-Host "`nChoice"
+    	switch ($ScriptSettingsMM) {
+            0 {Return 0}
+            1 {Return 1}
+            2 {Return 2}
+            3 {if($Number -ge 3) {Return 3} Else {$Invalid = 1}}
+            4 {if($Number -eq 4) {Return 4} Else {$Invalid = 1}}
+            C {Return $VariV} 
+            default {$Invalid = 1}
+		}
+    } 
+}
 
-$SettingFileItems = @(
-'                   Setting File                   ',
-'                                                 ',
-'            Please Input Filename.               ',
-'0. Cancel/Back to Main Menu                       '
-)
+##########
+# Multi Use Functions -End
+##########
+
+##########
+# Confirm Menu -Start
+##########
 
 $ConfirmMenuItems1 = @(
 '                  Confirm Dialog                  ',
@@ -101,75 +112,27 @@ $ConfirmMenuItems2 = @(
 '0. Cancel/Back to Main Menu                       '
 )
 
-$HelpItems = @(
-'                       Help                       ',
-'                                                 ',
-' Basic Usage:                                    ',
-' Use the menu & select what you want to change.  ',
-'                                                 ',
-' Advanced Usage Choices (Bypasses Menu):         ',
-' 1. Edit the script & change values there then   ',
-'        run script with "-set Run"               ',
-' 2. Run Script with importing a file with        ',
-'        "-set filename"                          ',
-' 3. Run Script with Window Default Values with   ',
-'        "-set WD" or "-set WindowsDefault"       ',
-'                                                 ',
-' Examples:                                       ',
-'    Win10-Mod.ps1 -set Run                       ',
-'    Win10-Mod.ps1 -set Settings.xml              ',
-'    Win10-Mod.ps1 -set WD                        ',
-'                                                 ',
-'Press "Enter" to go back                          '
-)
-
-$AboutItems = @(
-'                About this Script                 ',
-'                                                 ',
-' This script makes it easier to setup an         ',
-' existing or new install with modded setting.    ',
-'                                                 ',
-' This script was made by Me (Madbomb122).        ',
-'    https://github.com/madbomb122/Win10Script    ',
-'                                                 ',
-' Original basic script was made by Disassembler  ',
-'    https://github.com/Disassembler0/            ',
-'                                                 ',
-"Press 'Enter' to go back                          "
-)
-
-$CopyrightItems = @(
-'                     Copyright                    ',
-'                                                 ',
-' Copyright (c) 2017 Disassembler -Original       ',
-' Copyright (c) 2017 Madbomb122 -This Script      ',
-' This program is free software: you can          ',
-' redistribute it and/or modify This program is   ',
-' free software This program is free software:    ',
-' you can redistribute it and/or modify it under  ',
-' the terms of the GNU General Public License as  ',
-' published by the Free Software Foundation,      ',
-' version 3 of the License.                       ',
-'                                                 ',
-' This program is distributed in the hope that it ',
-' will be useful, but WITHOUT ANY WARRANTY;       ',
-' without even the implied warranty of            ',
-' MERCHANTABILITY or FITNESS FOR A PARTICULAR     ', 
-' PURPOSE.  See the GNU General Public License    ', 
-' for more details.                               ',
-'                                                 ', 
-' You should have received a copy of the GNU      ',
-' General Public License along with this program. ',
-' If not, see <http://www.gnu.org/licenses/>.     '
-)
+function ConfirmMenu([int] $Option) {
+    $ConfirmMenu = 'X'
+    while($ConfirmMenu -ne "Out"){
+        Clear-Host
+		If ($Option -eq 1){
+			VariableDisplay $ConfirmMenuItems1
+		} ElseIf ($Option -eq 2){
+			VariableDisplay $ConfirmMenuItems2
+		}
+		$ConfirmMenu = Read-Host "`nSelection (Y/N)"
+        switch (ConfirmMenu) {
+			Y {Return $true}
+            N {Return $false} 
+		    default {Return $false}
+		}
+    } 
+}
 
 ##########
-# Menu Display Items -End
+# Confirm Menu -End
 ##########
-
-# Used to get all values BEFORE any defined so 
-# when exporting shows ALL defined after this point
-$AutomaticVariables = Get-Variable
 
 ##########
 # Menu Display Function -Start
@@ -244,6 +207,16 @@ function Website {
 # Main Menu -Start
 ##########
 
+$MainMenuItems = @(
+'                    Main Menu                     ',
+'1. Run Script          ','H. Help                ',
+'2. Script Settings     ','C. Copyright           ',
+'3. Load Setting        ','A. About/Version       ',
+'4. Save Setting        ','                       ',
+'5. Script Options      ','                       ',
+'Q. Exit/Quit                                      '
+)
+
 function mainMenu {
     $mainMenu = 'X'
     while($mainMenu -ne "Out"){
@@ -277,6 +250,17 @@ function mainMenu {
 ##########
 # Script Settings -Start
 ##########
+
+$ScriptSettingsMainMenuItems = @(
+'             Script Setting Main Menu             ',
+'1. Privacy Settings    ','7. Windows Update      ',
+'2. Service Tweaks      ','8. Context Menu        ',
+'3. Start Menu          ','9. Task Bar            ',
+'4. Lock Screen         ','10. Features           ',
+"5. 'This PC'           ",'11. Metro Apps         ',
+'6. Explorer            ','12. Misc/Photo Viewer  ',
+'B. Back to Main Menu                              '
+)
 
 function ScriptSettingsMM {
     $ScriptSettingsMM = 'X'
@@ -313,8 +297,15 @@ function ScriptSettingsMM {
 ##########
 
 ##########
-# Load Settings -Start
+# Load/Save Settings -Start
 ##########
+
+$SettingFileItems = @(
+'                   Setting File                   ',
+'                                                 ',
+'            Please Input Filename.               ',
+'0. Cancel/Back to Main Menu                       '
+)
 
 function LoadSetting {
     $LoadSetting = 'X'
@@ -344,14 +335,6 @@ function LoadSetting {
     }
 }
 
-##########
-# Load Settings -End
-##########
-
-##########
-# Save Settings -Start
-##########
-
 function SaveSetting {
     $SaveSetting = 'X'
     while($SaveSetting -ne "Out"){
@@ -375,12 +358,34 @@ function SaveSetting {
 }
 
 ##########
-# Save Settings -End
+#  Load/Save Settings -End
 ##########
 
 ##########
 # Info Display Stuff -Start
 ##########
+
+$HelpItems = @(
+'                       Help                       ',
+'                                                 ',
+' Basic Usage:                                    ',
+' Use the menu & select what you want to change.  ',
+'                                                 ',
+' Advanced Usage Choices (Bypasses Menu):         ',
+' 1. Edit the script & change values there then   ',
+'        run script with "-set Run"               ',
+' 2. Run Script with importing a file with        ',
+'        "-set filename"                          ',
+' 3. Run Script with Window Default Values with   ',
+'        "-set WD" or "-set WindowsDefault"       ',
+'                                                 ',
+' Examples:                                       ',
+'    Win10-Mod.ps1 -set Run                       ',
+'    Win10-Mod.ps1 -set Settings.xml              ',
+'    Win10-Mod.ps1 -set WD                        ',
+'                                                 ',
+'Press "Enter" to go back                          '
+)
 
 function HelpMenu {
     $HelpMenu = 'X'
@@ -394,6 +399,21 @@ function HelpMenu {
     }
 }
 
+$AboutItems = @(
+'                About this Script                 ',
+'                                                 ',
+' This script makes it easier to setup an         ',
+' existing or new install with modded setting.    ',
+'                                                 ',
+' This script was made by Me (Madbomb122).        ',
+'    https://github.com/madbomb122/Win10Script    ',
+'                                                 ',
+' Original basic script was made by Disassembler  ',
+'    https://github.com/Disassembler0/            ',
+'                                                 ',
+"Press 'Enter' to go back                          "
+)
+
 function AboutMenu {
     $AboutMenu = 'X'
     while($AboutMenu -ne "Out"){
@@ -405,6 +425,31 @@ function AboutMenu {
         }
     }
 }
+
+$CopyrightItems = @(
+'                     Copyright                    ',
+'                                                 ',
+' Copyright (c) 2017 Disassembler -Original       ',
+' Copyright (c) 2017 Madbomb122 -This Script      ',
+' This program is free software: you can          ',
+' redistribute it and/or modify This program is   ',
+' free software This program is free software:    ',
+' you can redistribute it and/or modify it under  ',
+' the terms of the GNU General Public License as  ',
+' published by the Free Software Foundation,      ',
+' version 3 of the License.                       ',
+'                                                 ',
+' This program is distributed in the hope that it ',
+' will be useful, but WITHOUT ANY WARRANTY;       ',
+' without even the implied warranty of            ',
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR     ', 
+' PURPOSE.  See the GNU General Public License    ', 
+' for more details.                               ',
+'                                                 ', 
+' You should have received a copy of the GNU      ',
+' General Public License along with this program. ',
+' If not, see <http://www.gnu.org/licenses/>.     '
+)
 
 function CopyrightMenu {
     $AboutMenu = 'X'
@@ -423,61 +468,119 @@ function CopyrightMenu {
 ##########
 
 ##########
-# Multi Use Functions -Start
+# Script Settings Sub Menu -Start
 ##########
 
-# Used to Help remove the Automatic variables
-function cmpv {
-    Compare-Object (Get-Variable) $AutomaticVariables -Property Name -PassThru | Where -Property Name -ne "AutomaticVariables"
-}
+$PrivacySetMenuItems = @(
+'              Privacy Settings Menu               ',
+'1. Telemetry           ','7. Wi-Fi Sense         ',
+'2. Smart Screen        ','8. Auto Logger File    ',
+'3. Location Tracking   ','9. Feedback            ',
+'4. Explorer            ','10. Advertising ID     ',
+'5. Cortana             ','11. Cortana Search     ',
+'6. Error Reporting     ','12. WAP Push           ',
+'B. Back to Script Setting Main Menu               '
+)
 
-function ChoicesMenu($Vari,$Number) {
-    $VariJ = -join($Vari,"Items")
-    $VariV = Get-Variable $Vari -valueOnly #Variable
-    $VariA = Get-Variable $VariJ -valueOnly #Array
-    $ChoicesMenu = 'X'
-    while($ChoicesMenu -ne "Out"){
-        Clear-Host
-		If($Invalid -eq 1){
-		    Write-host ""
-			Write-host "Invalid Selection" -ForegroundColor Red -BackgroundColor Black -NoNewline
-			$Invalid = 0
-		}
-		ChoicesDisplay $VariA $VariV
-        $ChoicesMenu = Read-Host "`nChoice"
-    	switch ($ScriptSettingsMM) {
-            0 {Return 0}
-            1 {Return 1}
-            2 {Return 2}
-            3 {if($Number -ge 3) {Return 3} Else {$Invalid = 1}}
-            4 {if($Number -eq 4) {Return 4} Else {$Invalid = 1}}
-            C {Return $VariV} 
-            default {$Invalid = 1}
-		}
-    } 
-}
+$WindowsUpdateSetMenuItems = @(
+'           Window Update Settings Menu            ',
+'1. Check for Update    ','5. Update Type         ',
+'2. Update Download     ','6. Update MSRT         ',
+'3. Update Driver       ','7. Restart on Update   ',
+'4. App Auto Download   ','                       ',
+'B. Back to Script Setting Main Menu               '
+)
 
-function ConfirmMenu([int] $Option) {
-    $ConfirmMenu = 'X'
-    while($ConfirmMenu -ne "Out"){
-        Clear-Host
-		If ($Option -eq 1){
-			VariableDisplay $ConfirmMenuItems1
-		} ElseIf ($Option -eq 2){
-			VariableDisplay $ConfirmMenuItems2
-		}
-		$ConfirmMenu = Read-Host "`nSelection (Y/N)"
-        switch (ConfirmMenu) {
-			Y {Return $true}
-            N {Return $false} 
-		    default {Return $false}
-		}
-    } 
-}
+$ServiceTweaksSetMenuItems = @(
+'           Service Tweaks Settings Menu           ',
+'1. User Agent Control  ','5. Sharing Mapped Drive',
+'2. Admin Shares        ','6. Firewall            ',
+'3. Windows Defender    ','7. HomeGroups          ',
+'4. Remote Assistance   ','8. Remote Desktop      ',
+'B. Back to Script Setting Main Menu               '
+)
+
+$ContextMenuSetMenuItems = @(
+'              Context Menu Items Menu             ',
+'1. Cast to Device      ','4. Previous Versions   ',
+'2. Include in Library  ','5. Pin To              ',
+'3. Share With          ','6. Send To             ',
+'B. Back to Script Setting Main Menu               '
+)
+
+$StartMenuSetMenuItems = @(
+'               Start Menu Items Menu              ',
+'1. Startmenu Web Search','3. Most Used Apps      ',
+'2. Suggestions         ','4. Recent & Frequent   ',
+'B. Back to Script Setting Main Menu               '
+)
+
+$TaskbarSetMenuItems = @(
+'                Taskbar Items Menu                ',
+'1. Battery UI Bar      ','7. Clock UI Bar        ',
+'2. Volume Control Bar  ','8. Taskbar Search Box  ',
+'3. Task View Button    ','9. Taskbar Icon Size   ',
+'4. Taskbar Grouping    ','10. Notifcation Icons  ',
+'5. Seconds in Clock    ','11. Last Active Click  ',
+'6. Multi Display       ','12. Buttons on Display ',
+'B. Back to Script Setting Main Menu               '
+)
+
+$ExplorerSetMenuItems = @(
+'               Explorer Items Menu                ',
+'1. Frequent Quick Acess','7. Content While Drag  ',
+'2. Recent Quick Acess  ','8. Explorer Open Locat ',
+'3. System Files        ','9. Known Extensions    ',
+'4. Hidden Files        ','10. Autorun            ',
+'5. Aero Snape          ','11. Autoplay           ',
+'6. Aero Shake          ','12. Pid in Title Bar   ',
+'B. Back to Script Setting Main Menu               '
+)
+
+$ThisPCSetMenuItems = @(
+"              'This PC' Items Menu                ",
+"1. 'This PC' On Desktop",'5. Desktop Icon        ',
+'2. Documents Icon      ','6. Downloads Icon      ',
+'3. Music Icon          ','7. Pictures Icon       ',
+'4. Videos Icon         ','                       ',
+'B. Back to Script Setting Main Menu               '
+)
+
+$LockScreenSetMenuItems = @(
+'              Lock Screen Items Menu              ',
+'1. Lock Screen         ','3. Lock Screen Alt     ',
+'2. Power Menu          ','4. Camera              ',
+'B. Back to Script Setting Main Menu               '
+)
+
+$MiscSetMenuItems = @(
+'           Misc/Photo Viewer Items Menu           ',
+'       Misc Item       ',' Photo Viewer Settings ',
+'1. Action Center       ','8. File Association    ',
+'2. Sticky Key Prompt   ','9. Open With Menu      ',
+'3. Numblock On Start   ','                       ',
+'4. F8 Boot Menu        ','                       ',
+'5. Remote UAC Token    ','                       ',
+'6. Hibernate           ','                       ',
+'7. Sleep               ','                       ',
+'B. Back to Script Setting Main Menu               '
+)
+
+$FeaturesAppsMenuItems = @(
+'                Features Items Menu               ',
+'1. One Drive           ','4. Media Player        ',
+'2. One Drive Install   ','5. Work Folders        ',
+'3. Xbox DVR            ','6. Linux Subsystem     ',
+'B. Back to Script Setting Main Menu               '
+)
 
 ##########
-# Multi Use Functions -End
+# Script Settings Sub Menu -End
 ##########
 
+
+# Used to get all values BEFORE any defined so 
+# when exporting shows ALL defined after this point
+$AutomaticVariables = Get-Variable
 
 mainMenu
