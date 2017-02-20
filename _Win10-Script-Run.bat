725d9120bf38ee9f4544350a96936d32e6b27c02
madbomb122 <madbomb122@users.noreply.github.com>
2017-02-20 11:34:09 -0800

    Update _Win10-Script-Run.bat

--- _Win10-Script-Run.bat
+++ _Win10-Script-Run.bat
@@ -1,10 +1,12 @@
 @ECHO OFF
-Echo "Running Win10 Initial Setup Script -Mod version"
-
+Echo "Running Win10 Initial Setup Script -Menu version"
 ::Add colons before the pause if you dont want a pause (or delete it)
 Pause 
 
-:: Remove the 2 Colons infrom of the command you want to run
+:: Remove the 2 Colons infront of the command you want to run
+
+:: First Line Description of Command
+:: Second Line Command
 
 ::Run script with Import of setting file Mine.txt (Bat file in same directory as Win10-Menu.ps1 & Mine.txt)
 ::PowerShell.exe -NoProfile -Command "& {Start-Process PowerShell.exe -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File %~dp0Win10-Menu.ps1 -Set %~dp0Mine.txt' -Verb RunAs}"
