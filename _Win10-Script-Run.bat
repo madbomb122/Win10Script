c009fe8a1c7ea76d939fcefcbaeedaacb75d3455
madbomb122 <madbomb122@users.noreply.github.com>
2017-03-03 22:44:43 -0800

    Update _Win10-Script-Run.bat

--- _Win10-Script-Run.bat
+++ _Win10-Script-Run.bat
@@ -3,19 +3,29 @@ Echo "Running Win10 Initial Setup Script -Menu version"
 ::Add colons before the pause if you dont want a pause (or delete it)
 Pause 
 
-:: Remove the 2 Colons infront of the command you want to run
+::----------------------------------------------------------------------
 
-:: First Line Description of Command
-:: Second Line Command
+:: Instructions
+:: Remove the 2 Colons infront of "The Command" you want to run
+:: Bat file MUST be in same directory as script
+:: Setting file MUST be in same directory as script
 
-::Run script with Import of setting file Mine.txt (Bat file in same directory as Win10-Menu.ps1 & Mine.txt)
-::PowerShell.exe -NoProfile -Command "& {Start-Process PowerShell.exe -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File %~dp0Win10-Menu.ps1 -Set %~dp0Mine.txt' -Verb RunAs}"
+:: Format of File
+:: Description of "The Command"
+:: "The Command"
+:: Note (If any)
 
-::Run script with Windows Default Settings  (Bat file in same directory as Win10-Menu.ps1)
-::PowerShell.exe -NoProfile -Command "& {Start-Process PowerShell.exe -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File %~dp0Win10-Menu.ps1 -Set WD' -Verb RunAs}"
+::----------------------------------------------------------------------
 
-::Run script with Settings in Script  (Bat file in same directory as Win10-Menu.ps1)
+::Run script with going into the Menu
+::PowerShell.exe -NoProfile -Command "& {Start-Process PowerShell.exe -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File %~dp0Win10-Menu.ps1' -Verb RunAs}"
+
+::Run script with Settings that are in the Script
 ::PowerShell.exe -NoProfile -Command "& {Start-Process PowerShell.exe -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File %~dp0Win10-Menu.ps1' -Set Run -Verb RunAs}"
 
-::Run script with going into the menu, For you to do settings (Bat file in same directory as Win10-Menu.ps1)
-::PowerShell.exe -NoProfile -Command "& {Start-Process PowerShell.exe -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File %~dp0Win10-Menu.ps1' -Verb RunAs}"
+::Run script with Windows Default Settings
+::PowerShell.exe -NoProfile -Command "& {Start-Process PowerShell.exe -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File %~dp0Win10-Menu.ps1 -Set WD' -Verb RunAs}"
+
+::Run script with Import of setting file Mine.txt
+::PowerShell.exe -NoProfile -Command "& {Start-Process PowerShell.exe -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File %~dp0Win10-Menu.ps1 -Set %~dp0Mine.txt' -Verb RunAs}"
+::Note: If your setting file is a different name make sure to change it from "Mine.txt" to your filename
