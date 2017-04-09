8bbbb3514ed4b64bd6e4aa1164e6905628888ec5
madbomb122 <madbomb122@users.noreply.github.com>
2017-04-08 22:33:23 -0700

    Update _Win10-Script-Run.bat

--- _Win10-Script-Run.bat
+++ _Win10-Script-Run.bat
@@ -4,11 +4,13 @@
 :: Bat, Script & Setting File (if used) MUST be in same Folder
 :: Change RunOption to = what you want to run (just the number)
 
-set Run_Option=1
-:: 1 = Run with Menu
-:: 2 = Run with Settings in Script File
-:: 3 = Run with Windows Default (Default Settings in script is to Skip, you have to change them)
-:: 4 = Run with Setting file (Change file name bellow)
+set Run_Option=0
+:: Anything other than following does nothing
+:: 0 = Run with Menu
+:: 1 = Run with Settings in Script File
+:: 2 = Run with Windows Default (Default Settings in script is to Skip, you have to change them)
+:: 3 = Run with Setting file (Change file name bellow)
+:: 4 = Exit (Dont Run script)
 
 :: Change Mine.xml to your setting file (if you have one)
 set Setting_File=Mine.xml
@@ -23,10 +25,11 @@ set Script_Path=%Script_Directory%%Script_File%
 ::----------------------------------------------------------------------
 set Use_Arg=yes
 
-if /i %Run_Option%==1 set Use_Arg=no
-if /i %Run_Option%==2 set Arg=Run
-if /i %Run_Option%==3 set Arg=WD
-if /i %Run_Option%==4 set Arg=%Setting_File%
+if /i %Run_Option%==0 set Use_Arg=no
+if /i %Run_Option%==1 set Arg=Run
+if /i %Run_Option%==2 set Arg=WD
+if /i %Run_Option%==3 set Arg=%Setting_File%
+if /i %Run_Option%==4 Exit
 
 SETLOCAL ENABLEDELAYEDEXPANSION
 if /i %Use_Arg%==no (
