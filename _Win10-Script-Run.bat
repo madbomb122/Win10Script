1ed41ed6a794a2aa5f92681a550b3a55273afa84
madbomb122 <madbomb122@users.noreply.github.com>
2017-04-11 16:48:03 -0700

    Update _Win10-Script-Run.bat

--- _Win10-Script-Run.bat
+++ _Win10-Script-Run.bat
@@ -4,7 +4,7 @@
 :: Bat, Script & Setting File (if used) MUST be in same Folder
 :: Change RunOption to = what you want to run (just the number)
 
-set Run_Option=0
+Set Run_Option=0
 :: Anything other than following does nothing
 :: 0 = Run with Menu
 :: 1 = Run with Settings in Script File
@@ -13,32 +13,33 @@ set Run_Option=0
 :: 4 = Exit (Dont Run script)
 
 :: Change Mine.xml to your setting file (if you have one)
-set Setting_File=Mine.xml
-set Script_File=Win10-Menu.ps1
+Set Setting_File=Mine.xml
+Set Script_File=Win10-Menu.ps1
 
 :: Do not change unless you know what you are doing
-set Script_Directory=%~dp0
-set Script_Path=%Script_Directory%%Script_File%
-
+Set Script_Directory=%~dp0
+Set Script_Path=%Script_Directory%%Script_File%
 
 :: DO NOT CHANGE ANYTHING PAST THIS LINE
 ::----------------------------------------------------------------------
-set Use_Arg=yes
+Set Use_Arg=yes
 
-if /i %Run_Option%==0 set Use_Arg=no
-if /i %Run_Option%==1 set Arg=Run
-if /i %Run_Option%==2 set Arg=WD
-if /i %Run_Option%==3 set Arg=%Setting_File%
-if /i %Run_Option%==4 Exit
+If /i %Run_Option%==0 Set Use_Arg=no
+If /i %Run_Option%==1 Set Arg=Run
+If /i %Run_Option%==2 Set Arg=WinDefault
+If /i %Run_Option%==3 Set Arg=%Setting_File%
+If /i %Run_Option%==4 Exit
 
 SETLOCAL ENABLEDELAYEDEXPANSION
-if /i %Use_Arg%==no (
+If /i %Use_Arg%==no (
+    echo "Running !Script_File!, with Menu option"
     powershell.exe -noprofile -ExecutionPolicy Bypass -command "&{start-process powershell -ArgumentList '-NoProfile -ExecutionPolicy Bypass -file \"!Script_Path!\"' -verb RunAs}"
 )
 ENDLOCAL DISABLEDELAYEDEXPANSION
 
 SETLOCAL ENABLEDELAYEDEXPANSION
-if /i %Use_Arg%==yes (
+If /i %Use_Arg%==yes (
+    echo "Running !Script_File! -Set !Arg!"
     PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& {Start-Process PowerShell -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File \"!Script_Path!\" -Set \"!Arg!\"' -Verb RunAs}";
 )
 ENDLOCAL DISABLEDELAYEDEXPANSION
