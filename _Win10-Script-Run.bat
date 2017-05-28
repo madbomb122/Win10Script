c49768c1ebf94643813ec55702f61d5f40165c96
madbomb122 <madbomb122@gmail.com>
2017-05-28 12:33:42 -0700

    Update _Win10-Script-Run.bat

--- _Win10-Script-Run.bat
+++ _Win10-Script-Run.bat
@@ -1,45 +1,71 @@
 @ECHO OFF
-
 :: Instructions
-:: Bat, Script & Setting File (if used) MUST be in same Folder
-:: Change RunOption to = what you want to run (just the number)
+:: Bat, Script, and setting MUST be in same Folder
+:: Change Option to = one of the listed options (mostly yes or no)
 
 Set Run_Option=0
 :: Anything other than following does nothing
-:: 0 = Run with Menu
-:: 1 = Run with Settings in Script File
-:: 2 = Run with Windows Default (Default Settings in script is to Skip, you have to change them)
-:: 3 = Run with Setting file (Change file name bellow)
-:: 4 = Exit (Dont Run script)
+:: 0 = Go to Script Menu
+:: 1 = Run Script with Settings in Script File
+:: 2 = Run Script with Windows Default
+:: 3 = Run Script with Your Setting file (Change file name bellow)
+:: 4 = Load Script with Windows Default (Does not run)
+:: 5 = Load Script with Your Setting file (Does not run)
 
-:: Change Mine.xml to your setting file (if you have one)
-Set Setting_File=Mine.xml
+:: Name of Script File
 Set Script_File=Win10-Menu.ps1
 
+:: Name of Setting File (Change Mine.xml to your setting file, if you have one)
+Set Setting_File=Mine.xml
+:: DO NOT HAVE SPACES IN FILENAME
+
+:: Change these to yes or no
+Set Accept_ToS=no
+:: no = See ToS
+:: yes = Skip ToS (You accepted it)
+
+Set Create_Restore_Point=no
+Set Restore_Point_Name=Win10_Initial_Setup_Script
+:: DO NOT HAVE SPACES IN NAME
+
+Set Restart_when_Done=yes
+
+:: Update Checks   
+:: If update is found it will Auto-download and use that (with your settings)       
+Set Script=no
+Set Internet_Check=yes 
+:: Internet_Check only matters If Script is yes and pings to github.com is blocked 
+
+:: Diagnostic Output (Wont run script)
+Set Diagnostic=no
+
+::----------------------------------------------------------------------
 :: Do not change unless you know what you are doing
 Set Script_Directory=%~dp0
 Set Script_Path=%Script_Directory%%Script_File%
 
 :: DO NOT CHANGE ANYTHING PAST THIS LINE
 ::----------------------------------------------------------------------
-Set Use_Arg=yes
-
-If /i %Run_Option%==0 Set Use_Arg=no
-If /i %Run_Option%==1 Set Arg=Run
-If /i %Run_Option%==2 Set Arg=WinDefault
-If /i %Run_Option%==3 Set Arg=%Setting_File%
-If /i %Run_Option%==4 Exit
-
 SETLOCAL ENABLEDELAYEDEXPANSION
-If /i %Use_Arg%==no (
-    echo "Running !Script_File!, with Menu option"
-    powershell.exe -noprofile -ExecutionPolicy Bypass -command "&{start-process powershell -ArgumentList '-NoProfile -ExecutionPolicy Bypass -file \"!Script_Path!\"' -verb RunAs}"
-)
-ENDLOCAL DISABLEDELAYEDEXPANSION
 
-SETLOCAL ENABLEDELAYEDEXPANSION
-If /i %Use_Arg%==yes (
-    echo "Running !Script_File! -Set !Arg!"
-    PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& {Start-Process PowerShell -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File \"!Script_Path!\" -Set \"!Arg!\"' -Verb RunAs}";
-)
+If /i %Run_Option%==1 Set Run_Option=!Run_Option! -run
+If /i %Run_Option%==2 Set Run_Option=!Run_Option! -run wd
+If /i %Run_Option%==3 Set Run_Option=!Run_Option! -run %Setting_File%
+If /i %Run_Option%==4 Set Run_Option=!Run_Option! -load wd
+If /i %Run_Option%==5 Set Run_Option=!Run_Option! -load %Setting_File%
+
+If /i %Accept_ToS%==yes Set Run_Option=!Run_Option! -atos
+
+If /i %Create_Restore_Point%==yes Set Run_Option=!Run_Option! -crp %Restore_Point_Name%
+
+If /i %Internet_Check%==no Set Run_Option=!Run_Option! -sic
+
+If /i %Script%==yes Set Run_Option=!Run_Option! -usc
+
+If /i %Service%==yes Set Run_Option=!Run_Option! -use
+
+If /i %Restart_when_Done%==no Set Run_Option=!Run_Option! -dnr
+
+echo "Running !Script_File!"
+PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& {Start-Process PowerShell -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File "!Script_Path! !Run_Option!"' -Verb RunAs}";
 ENDLOCAL DISABLEDELAYEDEXPANSION
